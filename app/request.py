4e827372603a9e50f277809d4fc47284a861e728
wanjiku <annritawanjiku77@gmail.com>
2022-06-12 23:11:28 +0300

    processing the results

--- app/request.py
+++ app/request.py
@@ -32,3 +32,33 @@ def get_article(category): #get article function which will tkae in the article 
             article_results = process_results(article_results_list)
 
     return article_results
+
+#function that will process the resutls and create article objects from the elements we need.
+
+def process_results(article_list): #the function process results takes in a list of dictionaries 
+    '''
+    funciton that porcess the article result and transform them to a list of objects
+    
+    Args:
+    article_list: a list of dictionaries that contain the article detials
+    
+    
+    Returns:
+    article_results: A list of movie objects
+    '''
+    article_results = [] #empty so that it can store the newly created article objects
+    for article_item in article_list: #loop the list of dictionaties using the get method to pass in the keys so we can access the values
+        id = article_item.get('id')
+        source = article_item.get('name')
+        title = article_item.get('title')
+        author = article_item.get('author')
+        description = article_item.get('description')
+        link = article_item.get('url')
+        image = article_item.get('urlToImage')
+        date = article_item.get('publishedAt')
+
+        if image: #check if some articles have images, if not they are not displayed
+            article_object = Article(id,source,title,author,description,link,image,date) #values gotten are added to the new article object
+            article_results.append(article_object) #new article object is appeded to the empty article results list
+
+    return article_results #return the list with the article objects
