091480508541e4777e5d1cef0f1190d07428f529
wanjiku <annritawanjiku77@gmail.com>
2022-06-15 07:02:14 +0300

    adding bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 # where aplication will be initialised
+#flask extensions need to be initialsied before they can be used for example bootstrap.
 from flask import Flask 
 from .config import DevConfig
+from flask_bootstrap import Bootstrap #import bootstrap class form the flaskbootstrap extension
+
 
 # initializing application
 app = Flask(__name__, instance_relative_config = True)  #creating app instance #instance helps to connedt to instance folder created
@@ -8,5 +11,8 @@ app = Flask(__name__, instance_relative_config = True)  #creating app instance #
 #setup configuration
 app.config.from_object(DevConfig) #set up configuration and pass the devconfig subclass
 app.config.from_pyfile('config.py') #connects to the config.py file in inatnce folerd and its contents are appended to app.config
+
+#init flask extension
+bootstrap = Bootstrap(app) #initialise thebootstrap class by passing in the app instance
 
 from app import views  #import views from app folder to create views
