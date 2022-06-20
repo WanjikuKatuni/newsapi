f890743a3588c1c8a3e98556f95d654bc57c4ac7
wanjiku <annritawanjiku77@gmail.com>
2022-06-20 08:26:18 +0300

    add search form and edit homepage

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@
 from flask import redirect, render_template, request, url_for #takes in the template file, searchers for it and loads it.
 from app import app #app instance imported from app folder.
 from .request import get_article, search_article #import the get articles function
-from .models import reviews
-from .forms import ReviewForm
+# from .models import reviews
+# from .forms import ReviewForm
 
-Review = reviews.Review
+# Review = reviews.Review
 
 
 #from newsapi import NewsApiClient
@@ -55,7 +55,7 @@ def index():
         return redirect(url_for('search', article_name = search_article))
     else:
 
-        return render_template('index.html', title = title, article = top_articles) #render templaete passes the index.html file created #first message is a vairable in the template, secodn message is the variabled in the view function/
+        return render_template('home.html', title = title, article = top_articles) #render templaete passes the index.html file created #first message is a vairable in the template, secodn message is the variabled in the view function/
 
 #add dynamic routes
 # @app.route('/article/<int:article_id>') #part in angle brackets is dynamic and are redenred as strings whcih can be transformed into any type use int to transform it to an int.
